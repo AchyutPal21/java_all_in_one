6ffe55640679eea9f8599b7d87b788344df20252
Achyut Pal <achyut21pal@gmail.com>
2026-04-29 18:21:52 +0530

    fixed quick sort edge cases

--- src/main/java/com/dsa/recursion/QuickSort.java
+++ src/main/java/com/dsa/recursion/QuickSort.java
@@ -4,50 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QuickSort {
-  private void swap(List<Integer> arr, int i, int j) {
-    int temp = arr.get(i);
-    arr.set(i, arr.get(j));
-    arr.set(j, temp);
-  }
 
-
-  private int fixPivotPosition(List<Integer> arr, int low, int high) {
-
-    int l = low; 
-    int h = high;
-
-    int pivot = l;
-
-    while (l <= h) {
-      while (l <= high && arr.get(l) <= arr.get(pivot)) {
-        l++;
-      }
-
-      while (h >= l && arr.get(h) > arr.get(pivot)) {
-        h--;
-      }
-
-      if (l < h) {
-        swap(arr, l, h);
-      }
+    private void swap(List<Integer> arr, int i, int j) {
+        int temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
     }
 
-    swap(arr, pivot, h);
+    private int fixPivotPosition(List<Integer> arr, int low, int high) {
+        int pivot = arr.get(low); // store pivot VALUE
+        int i = low;
+        int j = high;
 
-    return  h;
-  }
+        while (i < j) {
 
+            while (i <= high - 1 && arr.get(i) <= pivot) {
+                i++;
+            }
 
-  public void sort(List<Integer> arr, int low, int high) {
-    if (low >= high) return;
+            while (j >= low + 1 && arr.get(j) > pivot) {
+                j--;
+            }
 
-    int pivot = fixPivotPosition(arr, low, high);
-    sort(arr, low, pivot - 1);
-    sort(arr, pivot + 1, high);
-  }
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
 
-  public void makeCall() {
-    List<Integer> list = new ArrayList<>();
+        swap(arr, low, j); // place pivot in correct position
+        return j;
+    }
+
+    public void sort(List<Integer> arr, int low, int high) {
+        if (low >= high) return;
+
+        int pivotIndex = fixPivotPosition(arr, low, high);
+
+        sort(arr, low, pivotIndex - 1);
+        sort(arr, pivotIndex + 1, high);
+    }
+
+    public void makeCall() {
+        List<Integer> list = new ArrayList<>();
+
         list.add(4);
         list.add(6);
         list.add(2);
@@ -56,16 +55,18 @@ public class QuickSort {
         list.add(9);
         list.add(1);
         list.add(3);
+
         int low = 0;
         int high = list.size() - 1;
+
         sort(list, low, high);
-        // unsorted: 4 6 2 5 7 9 1 3
-        // sorted: 1 2 3 4 5 6 7 9
+
         System.out.println("Sorted Array:");
+
         for (int num : list) {
             System.out.print(num + " ");
         }
-        System.out.println();
-  }
 
+        System.out.println();
+    }
 }
