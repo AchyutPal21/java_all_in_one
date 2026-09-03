55492735715f2189769eb85f7bf5084be0cca334
Achyut Pal <achyutpal@Achyut-Pal.local>
2026-09-03 12:32:57 +0530

    Restart 03/09/2026

--- src/main/java/com/dsa/Main.java
+++ src/main/java/com/dsa/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         ClimbingStairsII q = new ClimbingStairsII();
         int n = 4;
-        int[] costs = new int[]{1,2,3,4};
+        int[] costs = new int[]{1,2,3,5};
         System.out.println("Min cost: " + q.climbStairs(n, costs));
         
     }
